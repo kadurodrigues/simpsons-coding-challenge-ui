860a9de82ce53e73ef92a11e580c60e3463bf843
Carlos Eduardo Rodrigues <carlosrodrigues@Carloss-MacBook-Pro.local>
2020-03-07 20:47:41 +0000

    Config server

--- client/src/app/components/character/character.module.ts
+++ client/src/app/components/character/character.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { CharacterComponent } from './character.component';
+import { CharacterService } from './character.service';
 
 @NgModule({
   declarations: [CharacterComponent],
@@ -9,6 +10,7 @@ import { CharacterComponent } from './character.component';
     CommonModule,
     SharedModule
   ],
-  exports: [CharacterComponent]
+  exports: [CharacterComponent],
+  providers: [CharacterService]
 })
 export class CharacterModule { }
